eddbbea6097a6d0c2c44065a624274de88040067
Browsers <38656592+nhNet@users.noreply.github.com>
2018-06-03 09:17:44 +0200

    Update instaTokens.js

--- private/AccessTokens/instaTokens.js
+++ private/AccessTokens/instaTokens.js
@@ -3,5 +3,9 @@
 // These will give data for the images that will show up...
 
 var NH_NET_AccessTokens = {
-	spbong999 : "spbong999@gmail.com___2332206178.1677ed0.cdf2a78e521849a7b980459a3d8bf4b9"
+	spbong999 : "2332206178.1677ed0.cdf2a78e521849a7b980459a3d8bf4b9"
+};
+
+var NH_NET_Email = {
+	spbong999 : "spbong999@gmail.com"
 };
