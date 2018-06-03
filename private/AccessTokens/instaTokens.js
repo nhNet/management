ff87fdde03008818b2c6953bfcc536430282ddc6
Browsers <38656592+nhNet@users.noreply.github.com>
2018-06-03 08:39:57 +0200

    Create instaTokens.js

---
+++ private/AccessTokens/instaTokens.js
@@ -0,0 +1,7 @@
+// Nicholas Huaman 2018
+// A file to store Instagram AccessTokens
+// These will give data for the images that will show up...
+
+var NH_NET_AccessTokens = {
+	spbong999 : "2332206178.1677ed0.cdf2a78e521849a7b980459a3d8bf4b9"
+};
