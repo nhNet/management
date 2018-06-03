5b68332f390731e8561d0fc21f930c0d00b1680b
Browsers <38656592+nhNet@users.noreply.github.com>
2018-06-03 09:35:02 +0200

    Update instaTokens.js

--- private/AccessTokens/instaTokens.js
+++ private/AccessTokens/instaTokens.js
@@ -6,6 +6,6 @@ var NH_NET_AccessTokens = {
 	spbong999 : "2332206178.1677ed0.cdf2a78e521849a7b980459a3d8bf4b9"
 };
 
-var NH_NET_Emails = {
-	spbong999 : "spbong999@gmail.com"
-};
+var NH_NET_Emails = [
+	"spbong999@gmail.com"
+];
