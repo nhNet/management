e7e59fad2093f1bed890c25a7f5fe024aed580e8
Browsers <38656592+nhNet@users.noreply.github.com>
2018-10-14 18:27:26 +0200

    Update instaTokens.js

--- private/AccessTokens/instaTokens.js
+++ private/AccessTokens/instaTokens.js
@@ -2,6 +2,6 @@
 // A file to store Instagram AccessTokens
 // These will give data for the images that will show up...
 
-var NH_NET_AccessTokens = {
-	spbong999 : "spbong999@gmail.com___2332206178.1677ed0.cdf2a78e521849a7b980459a3d8bf4b9"
-};
+var NH_NET_AccessTokens = [
+	"spbong999@gmail.com___2332206178.1677ed0.cdf2a78e521849a7b980459a3d8bf4b9"
+];
